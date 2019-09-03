4d62855102e128aa69dd47d31916c85f5db34904
oleg-student <thinkmoretodomore@gmail.com>
2019-09-03 23:59:03 +0300

    lambda expressions everywhere 2

--- source/sort.js
+++ source/sort.js
@@ -3,18 +3,17 @@
 const sort = (text) => {
     // trim spaces at the beginning and at the end
     // of passed text
-    var trimmedText = text.trim();
+    const trimmedText = text.trim();
     if (trimmedText === '')
         return text;
-    var a = trimmedText.split(/\s+/);
-    var collator = new Intl.Collator();
-    a = a.map((value) => {
+    const result = trimmedText.split(/\s+/);
+    const collator = new Intl.Collator();
+    return result.map((value) => {
         value = value.toLowerCase()
             .split('')
             .sort(collator.compare);
         value[0] = value[0].toUpperCase();
         return value.join('');
-    });
-    a = a.sort(collator.compare).join(' ');
-    return a;
+    }).sort(collator.compare)
+        .join(' ');
 }
